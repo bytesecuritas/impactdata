4dce9fa1a0b1b3af1e0385f6bbfe2b3edacade51
Morymirco <morykoulibaly996@gmail.com>
2025-06-30 15:51:17 +0000

    render

--- build.sh
+++ build.sh
@@ -5,11 +5,22 @@ set -o errexit
 # Mettre à jour pip
 pip install --upgrade pip
 
-# Installer les dépendances système nécessaires
-# (Render les installe automatiquement, mais c'est une bonne pratique)
+# Installer les dépendances système nécessaires pour Pillow
+# (Render les installe automatiquement)
 
-# Installer les dépendances Python
-pip install -r requirements.txt
+# Installer les dépendances Python avec des versions compatibles
+pip install Django==4.2.23
+pip install Pillow==10.4.0
+pip install qrcode==7.4.2
+pip install reportlab==4.0.7
+pip install gunicorn==21.2.0
+pip install whitenoise==6.6.0
+pip install dj-database-url==2.1.0
+pip install psycopg2-binary==2.9.9
+pip install python-decouple==3.8
+pip install asgiref==3.7.2
+pip install sqlparse==0.4.4
+pip install tzdata==2023.3
 
 # Collecter les fichiers statiques
 python manage.py collectstatic --no-input

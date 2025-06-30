b8225569c9316b475f9040426a999b7c9bc84650
Morymirco <morykoulibaly996@gmail.com>
2025-06-30 15:43:48 +0000

    render

--- build.sh
+++ build.sh
@@ -2,7 +2,17 @@
 # exit on error
 set -o errexit
 
+# Mettre à jour pip
+pip install --upgrade pip
+
+# Installer les dépendances système nécessaires
+# (Render les installe automatiquement, mais c'est une bonne pratique)
+
+# Installer les dépendances Python
 pip install -r requirements.txt
 
+# Collecter les fichiers statiques
 python manage.py collectstatic --no-input
+
+# Effectuer les migrations
 python manage.py migrate 
